a3498459bc5b431ce769d40bf21f4d6937ac10f7
lisp719 <368034+lisp719@users.noreply.github.com>
2022-03-23 15:13:09 +0900

    refactor: Remove unnecessary attr_reader

--- spec/mogu/prompt_spec.rb
+++ spec/mogu/prompt_spec.rb
@@ -4,97 +4,88 @@ RSpec.describe Mogu::Prompt do
   describe '#run' do
     subject { described_class.new }
 
-    let(:template) { Mogu::Template.new }
-
     before do
-      allow(subject).to receive(:app_path).and_return('app_path')
-      allow(subject).to receive(:customizes).and_return(%w[database javascript css gems])
-      allow(subject).to receive(:database).and_return('database')
-      allow(subject).to receive(:javascript).and_return('javascript')
-      allow(subject).to receive(:css).and_return('css')
-      allow(subject).to receive(:gems).and_return(%w[brakeman rspec])
-      allow(Mogu::Template).to receive(:create).and_return(template)
+      allow(subject).to receive(:database?).and_return(true)
+      allow(subject).to receive(:javascript?).and_return(true)
+      allow(subject).to receive(:css?).and_return(true)
+      allow(subject).to receive(:gems?).and_return(true)
+      allow(subject).to receive(:template?).and_return(true)
+
+      allow(subject).to receive(:app_path)
+      allow(subject).to receive(:customizes)
+      allow(subject).to receive(:database)
+      allow(subject).to receive(:javascript)
+      allow(subject).to receive(:css)
+      allow(subject).to receive(:gems)
+      allow(Mogu::Template).to receive(:create)
+
+      subject.run
     end
 
-    context 'before method execution' do
-      let(:expected) do
-        { app_path: '', customizes: [], database: '', javascript: '', css: '', gems: [], template: nil }
-      end
+    it { is_expected.to have_received(:app_path) }
+    it { is_expected.to have_received(:customizes) }
+    it { is_expected.to have_received(:database) }
+    it { is_expected.to have_received(:javascript) }
+    it { is_expected.to have_received(:css) }
+    it { is_expected.to have_received(:gems) }
 
-      it { expect(subject.result.to_h).to eq expected }
-    end
-
-    context 'after method execution' do
-      before { subject.run }
-
-      let(:expected) do
-        {
-          app_path: 'app_path',
-          customizes: %w[database javascript css gems],
-          database: 'database',
-          javascript: 'javascript',
-          css: 'css',
-          gems: %w[brakeman rspec],
-          template: template
-        }
-      end
-
-      it { expect(subject.result.to_h).to eq expected }
-    end
+    it { expect(Mogu::Template).to have_received(:create) }
   end
 
   describe '#to_opt' do
-    subject { described_class.new }
+    subject { described_class.new.to_opt }
+
+    let(:result) do
+      Mogu::Prompt::Result.new(
+        app_path: 'app_path',
+        customizes: customizes,
+        database: 'sqlite3',
+        javascript: 'importmap',
+        css: 'tailwind',
+        gems: gems,
+        template: template
+      )
+    end
 
     let(:template) { Mogu::Template.new }
 
     before do
-      subject.result.app_path = 'app_path'
+      allow(Mogu::Prompt::Result).to receive(:new).and_return(result)
     end
 
     context 'only app_path ' do
-      it { expect(subject.to_opt).to eq ['app_path'] }
+      let(:customizes) { [] }
+      let(:gems) { [] }
+
+      it { is_expected.to eq ['app_path'] }
     end
 
     context 'with base options ' do
-      before do
-        subject.result.customizes = %w[database javascript css]
-        subject.result.database = 'database'
-        subject.result.javascript = 'javascript'
-        subject.result.css = 'css'
-      end
+      let(:customizes) { %w[database javascript css] }
+      let(:gems) { [] }
 
-      it { expect(subject.to_opt).to eq %w[app_path -d database -j javascript -c css] }
+      it { is_expected.to eq %w[app_path -d sqlite3 -j importmap -c tailwind] }
     end
 
     context 'with brakeman' do
-      before do
-        subject.result.customizes = %w[gems]
-        subject.result.gems = %w[brakeman]
-        subject.result.template = template
-      end
+      let(:customizes) { ['gems'] }
+      let(:gems) { ['brakeman'] }
 
-      it { expect(subject.to_opt).to eq ['app_path', '-m', template.file.path] }
+      it { is_expected.to eq ['app_path', '-m', template.path] }
     end
 
     context 'with rspec' do
-      before do
-        subject.result.customizes = %w[gems]
-        subject.result.gems = %w[rspec]
-        subject.result.template = template
-      end
+      let(:customizes) { ['gems'] }
+      let(:gems) { ['rspec'] }
 
-      it { expect(subject.to_opt).to eq ['app_path', '-T', '-m', template.file.path] }
+      it { is_expected.to eq ['app_path', '-T', '-m', template.path] }
     end
 
     context 'with rubocop' do
-      before do
-        subject.result.customizes = %w[gems]
-        subject.result.gems = %w[rubocop]
-        subject.result.template = template
-      end
+      let(:customizes) { ['gems'] }
+      let(:gems) { ['rubocop'] }
 
-      it { expect(subject.to_opt).to eq ['app_path', '-m', template.file.path] }
+      it { is_expected.to eq ['app_path', '-m', template.path] }
     end
   end
 end
